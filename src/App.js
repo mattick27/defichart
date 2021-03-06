68dbe67f974ca8c7e8638dd3dbde3091e490a233
matichoneasyrice <matichon.man@easyrice.ai>
2021-03-07 00:02:47 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
+import {useState , useEffect} from 'react'
+import { Chart } from 'react-charts'
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [data, setData] = useState(null)
+  const [axes, setAxes] = useState([
+    { primary: true, type: 'linear', position: 'bottom' },
+    { type: 'linear', position: 'left' }
+  ])
+
+  useEffect(() => {
+    fetch('http://54.254.93.168:1111/viewDataPointByContract?contract=0xca68100dc0cc00cfba894370b60cccceb0c6cb2c')
+    .then(res => res.json())
+    .then((newData)=>{
+      console.log(newData)
+      let tempData = []
+      let arrData = []
+      newData.map((dataValue,idx)=>{
+        arrData.push({
+          x : idx,
+          y : dataValue['Point']
+        })
+      })
+      tempData.push({
+        label : 'slime',
+        data : arrData
+      })
+      setData(tempData)
+    })
+  }, [])
+  if(data){
+    return (
+      <div className="App">
+  
+        <div
+          style={{
+            width: '400px',
+            height: '300px'
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          
+          <Chart data={data} axes={axes} />
+        </div>
+  
+      </div>
+    );
+  }else{
+    return <></>
+  }
+
 }
 
 export default App;

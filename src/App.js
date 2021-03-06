57ecb6a9cd0c0b3939c6519bee8ce0480cc2281d
matichoneasyrice <matichon.man@easyrice.ai>
2021-03-07 00:08:43 +0700

    add interval

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import {useState , useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import { Chart } from 'react-charts'
 
 import './App.css';
@@ -12,42 +12,45 @@ function App() {
   ])
 
   useEffect(() => {
-    fetch('http://54.254.93.168:1111/viewDataPointByContract?contract=0xca68100dc0cc00cfba894370b60cccceb0c6cb2c')
-    .then(res => res.json())
-    .then((newData)=>{
-      console.log(newData)
-      let tempData = []
-      let arrData = []
-      newData.map((dataValue,idx)=>{
-        arrData.push({
-          x : idx,
-          y : dataValue['Point']
+    setInterval(() => {
+      fetch('http://54.254.93.168:1111/viewDataPointByContract?contract=0xca68100dc0cc00cfba894370b60cccceb0c6cb2c')
+        .then(res => res.json())
+        .then((newData) => {
+          console.log(newData)
+          let tempData = []
+          let arrData = []
+          newData.map((dataValue, idx) => {
+            arrData.push({
+              x: idx,
+              y: dataValue['Point']
+            })
+          })
+          tempData.push({
+            label: 'slime',
+            data: arrData
+          })
+          setData(tempData)
         })
-      })
-      tempData.push({
-        label : 'slime',
-        data : arrData
-      })
-      setData(tempData)
-    })
+    }, 5000)
+
   }, [])
-  if(data){
+  if (data) {
     return (
       <div className="App">
-  
+
         <div
           style={{
             width: '400px',
             height: '300px'
           }}
         >
-          
+
           <Chart data={data} axes={axes} />
         </div>
-  
+
       </div>
     );
-  }else{
+  } else {
     return <></>
   }
 
